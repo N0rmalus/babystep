a694e4733b871e9022445d0cd5c862bb4b67b4b8
Rokas Kartenis <rokas.kartenis@cornercasetech.com>
2026-04-15 01:34:52 +0300

    Remove redundant stuff from category rework

--- actions/get-colors.tsx
+++
@@ -1,6 +0,0 @@
-import { apiGet } from '@/lib/api-client';
-import { Color } from '@/actions/types';
-
-const getColors = async (): Promise<Color[]> => apiGet<Color[]>('/colors');
-
-export default getColors;
--- actions/get-sizes.tsx
+++
@@ -1,6 +0,0 @@
-import { apiGet } from '@/lib/api-client';
-import { Size } from '@/actions/types';
-
-const getSizes = async (): Promise<Size[]> => apiGet<Size[]>('/sizes');
-
-export default getSizes;
