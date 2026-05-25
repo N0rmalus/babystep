ad657a6e021a162ad9b2fcae871472757687cd40
Rokas Kartenis <rokas.kartenis@cornercasetech.com>
2026-05-25 12:28:44 +0300

    Adjust product-list media queries

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -13,7 +13,7 @@ export const ProductList = ({ title, items }: Props) => {
       <h3 className="text-3xl leading-tight font-bold text-neutral-900 md:text-4xl">{title}</h3>
       {items.length === 0 && <NoResults />}
 
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-4 min-[420px]:grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
         {items.map((item) => (
           <ProductCard key={item.id} data={item} />
         ))}
