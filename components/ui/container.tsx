3fefb82b5ead77e9f1420e13bce2cc3162b8e5f2
Rokas Kartenis <rokas.kartenis@cornercasetech.com>
2026-06-01 11:25:37 +0300

    Tweak page container

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -8,9 +8,7 @@ interface Props {
 
 const Container = ({ children, className }: Props) => {
   return (
-    <div
-      className={cn('mx-auto flex max-w-7xl flex-col gap-8 px-4 py-10 sm:px-6 sm:py-14 lg:gap-10 lg:px-8', className)}
-    >
+    <div className={cn('mx-auto flex max-w-7xl flex-col gap-4 p-4 sm:px-6 sm:py-14 lg:gap-10 lg:px-8', className)}>
       {children}
     </div>
   );
